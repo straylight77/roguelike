f3f89990c24436beb0ae90be88f1f0cdc44857db
Coline Bowers <colin.bowers@gmail.com>
2024-02-19 18:53:55 -0500

    Small refactor.

--- go-ebiten/main.go
+++ go-ebiten/main.go
@@ -1,85 +1,56 @@
 package main
 
 import (
-	"image"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
-
-type Spritesheet struct {
-	TileSize   int
-	TileXCount int
-	Img        *ebiten.Image
-}
-
-func NewSpritesheet(fname string, tileSize int) *Spritesheet {
-	s := &Spritesheet{}
-
-	var err error
-	s.Img, _, err = ebitenutil.NewImageFromFile("assets/" + fname)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	s.TileSize = tileSize
-	w := s.Img.Bounds().Dx()      // width in pixels
-	s.TileXCount = w / s.TileSize // width in number of tiles
-
-	return s
-}
-
-func (s *Spritesheet) TileImg(id int) *ebiten.Image {
-	id = id - 1 // tile nubmers start at 1
-	sx := (id % s.TileXCount) * s.TileSize
-	sy := (id / s.TileXCount) * s.TileSize
-	rect := image.Rect(sx, sy, sx+s.TileSize, sy+s.TileSize)
-	return s.Img.SubImage(rect).(*ebiten.Image)
-}
-
-// -----------------------------------------------------------------------
 
 var sheet *Spritesheet
 
 type Game struct{}
 
+// -----------------------------------------------------------------------
 func NewGame() *Game {
 	g := &Game{}
 	return g
 }
 
+// -----------------------------------------------------------------------
 func (g *Game) Update() error {
 	return nil
 }
 
+// -----------------------------------------------------------------------
 func (g *Game) Draw(screen *ebiten.Image) {
 
 	max_x, max_y := 5, 5
 	dungeon := []int{
 		T_WALL, T_WALL, T_WALL, T_WALL, T_WALL,
-		T_WALL, T_FLOR, T_FLOR, T_STUP, T_WALL,
-		T_WALL, T_FLOR, T_FLOR, T_FLOR, T_DRCL,
-		T_WALL, T_FLOR, T_FLOR, T_FLOR, T_WALL,
-		T_WALL, T_WALL, T_DROP, T_WALL, T_WALL,
+		T_WALL, T_FLOOR, T_FLOOR, T_UP, T_WALL,
+		T_WALL, T_FLOOR, T_FLOOR, T_FLOOR, T_DOOR_CL,
+		T_WALL, T_RING, T_FLOOR, T_FLOOR, T_WALL,
+		T_WALL, T_WALL, T_DOOR_OP, T_WALL, T_WALL,
 	}
 	for x := 0; x < max_x; x++ {
 		for y := 0; y < max_y; y++ {
-			DrawTile(screen, sheet.TileImg(dungeon[x+(max_x*y)]), x+1, y+1)
+			DrawTile(screen, sheet.Tile(dungeon[x+(max_x*y)]), x+1, y+1)
 		}
 	}
 
-	DrawTile(screen, sheet.TileImg(T_HERO), 3, 3)
-	//DrawTile(screen, sheet.TileImg(T_PATH), 3, 6)
-	//DrawTile(screen, sheet.TileImg(T_PATH), 3, 7)
-	//DrawTile(screen, sheet.TileImg(T_PATH), 4, 7)
+	DrawTile(screen, sheet.Tile(T_HERO), 3, 3)
+	DrawTile(screen, sheet.Tile(T_PATH), 3, 6)
+	DrawTile(screen, sheet.Tile(T_PATH), 3, 7)
+	DrawTile(screen, sheet.Tile(T_PATH), 4, 7)
 }
 
+// -----------------------------------------------------------------------
 func (g *Game) Layout(outsideW, outsideH int) (screenWidth, screenHeight int) {
-	return 320, 240
-	//return 640, 480
+	//return 320, 240
+	return 640, 480
 }
 
+// -----------------------------------------------------------------------
 func DrawTile(screen *ebiten.Image, img *ebiten.Image, x int, y int) {
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(x*sheet.TileSize), float64(y*sheet.TileSize))
@@ -87,11 +58,12 @@ func DrawTile(screen *ebiten.Image, img *ebiten.Image, x int, y int) {
 
 }
 
+// -----------------------------------------------------------------------
 func main() {
 	sheet = NewSpritesheet("colored_packed.png", 16)
 	g := NewGame()
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Rogue Retro")
+	ebiten.SetWindowTitle("1-Bit Rogue")
 
 	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
--- go-ebiten/spritesheet.go
+++ go-ebiten/spritesheet.go
@@ -1,1 +1,79 @@
 package main
+
+import (
+	"image"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+// wall:		637
+// open door:   447
+// closed door: 444
+// player:		1021 (or 27)
+// stairs up:   297
+// stairs down: 296
+// path:		2
+// per row = 49
+
+const (
+	T_WALL    = 637
+	T_PATH    = 2
+	T_FLOOR   = 0
+	T_DOOR_OP = 447
+	T_DOOR_CL = 444
+	T_UP      = 297
+	T_DOWN    = 296
+	T_HERO    = 27
+	T_A       = 917
+	T_B       = 918
+	T_C       = 919
+	T_D       = 920
+	T_E       = 921
+	T_F       = 922
+	T_G       = 923
+	T_H       = 924
+	T_I       = 925
+	T_J       = 926
+	T_K       = 927
+	T_L       = 928
+	T_M       = 929
+	T_N       = 966
+	T_POTION  = 671
+	T_SCROLL  = 769
+	T_RING    = 337
+	T_WEAPON  = 0
+	T_ARMOR   = 0
+)
+
+type Spritesheet struct {
+	TileSize   int
+	TileXCount int
+	Img        *ebiten.Image
+}
+
+// -----------------------------------------------------------------------
+func NewSpritesheet(fname string, tileSize int) *Spritesheet {
+	img, _, err := ebitenutil.NewImageFromFile("assets/" + fname)
+	if err != nil {
+		log.Fatal(err)
+	}
+	width := img.Bounds().Dx() // width in pixels
+
+	s := &Spritesheet{
+		tileSize,
+		width / tileSize,
+		img,
+	}
+	return s
+}
+
+// -----------------------------------------------------------------------
+func (s *Spritesheet) Tile(id int) *ebiten.Image {
+	//id = id - 1 // tile nubmers start at 1
+	sx := (id % s.TileXCount) * s.TileSize
+	sy := (id / s.TileXCount) * s.TileSize
+	rect := image.Rect(sx, sy, sx+s.TileSize, sy+s.TileSize)
+	return s.Img.SubImage(rect).(*ebiten.Image)
+}
