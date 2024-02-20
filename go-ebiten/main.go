10af327eb4168384167144345158f4785c26cf51
Coline Bowers <colin.bowers@gmail.com>
2024-02-20 12:59:53 -0500

    Determined tile id's for each monster.

--- go-ebiten/main.go
+++ go-ebiten/main.go
@@ -24,30 +24,38 @@ func (g *Game) Update() error {
 // -----------------------------------------------------------------------
 func (g *Game) Draw(screen *ebiten.Image) {
 
-	max_x, max_y := 5, 5
+	//max_x, max_y := 5, 5
+	//dungeon := []int{
+	//	T_WALL, T_WALL, T_WALL, T_WALL, T_WALL,
+	//	T_WALL, T_FLOOR, T_FLOOR, T_UP, T_WALL,
+	//	T_WALL, T_FLOOR, T_FLOOR, T_FLOOR, T_DOOR_CL,
+	//	T_WALL, T_RING, T_FLOOR, T_FLOOR, T_WALL,
+	//	T_WALL, T_WALL, T_DOOR_OP, T_WALL, T_WALL,
+	//}
+
+	max_x, max_y := 26, 2
 	dungeon := []int{
-		T_WALL, T_WALL, T_WALL, T_WALL, T_WALL,
-		T_WALL, T_FLOOR, T_FLOOR, T_UP, T_WALL,
-		T_WALL, T_FLOOR, T_FLOOR, T_FLOOR, T_DOOR_CL,
-		T_WALL, T_RING, T_FLOOR, T_FLOOR, T_WALL,
-		T_WALL, T_WALL, T_DOOR_OP, T_WALL, T_WALL,
+		T_A, T_B, T_C, T_D, T_E, T_F, T_G, T_H, T_I, T_J, T_K, T_L, T_M,
+		T_N, T_O, T_P, T_Q, T_R, T_S, T_T, T_U, T_V, T_W, T_X, T_Y, T_Z,
+		269, 418, 275, 421, 374, 417, 272, 126, 369, 420, 177, 467, 324,
+		276, 466, 416, 127, 273, 419, 322, 270, 122, 367, 485, 325, 323,
 	}
 	for x := 0; x < max_x; x++ {
 		for y := 0; y < max_y; y++ {
-			DrawTile(screen, sheet.Tile(dungeon[x+(max_x*y)]), x+1, y+1)
+			DrawTile(screen, sheet.Tile(dungeon[x+(max_x*y)]), x, y)
 		}
 	}
 
-	DrawTile(screen, sheet.Tile(T_HERO), 3, 3)
-	DrawTile(screen, sheet.Tile(T_PATH), 3, 6)
-	DrawTile(screen, sheet.Tile(T_PATH), 3, 7)
-	DrawTile(screen, sheet.Tile(T_PATH), 4, 7)
+	//DrawTile(screen, sheet.Tile(T_HERO), 3, 3)
+	//DrawTile(screen, sheet.Tile(T_PATH), 3, 6)
+	//DrawTile(screen, sheet.Tile(T_PATH), 3, 7)
+	//DrawTile(screen, sheet.Tile(T_PATH), 4, 7)
 }
 
 // -----------------------------------------------------------------------
 func (g *Game) Layout(outsideW, outsideH int) (screenWidth, screenHeight int) {
-	//return 320, 240
-	return 640, 480
+	return 640, 240
+	//return 640, 480
 }
 
 // -----------------------------------------------------------------------
@@ -62,7 +70,7 @@ func DrawTile(screen *ebiten.Image, img *ebiten.Image, x int, y int) {
 func main() {
 	sheet = NewSpritesheet("colored_packed.png", 16)
 	g := NewGame()
-	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowSize(1280, 480)
 	ebiten.SetWindowTitle("1-Bit Rogue")
 
 	if err := ebiten.RunGame(g); err != nil {
